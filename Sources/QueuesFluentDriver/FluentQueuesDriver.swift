96df6a0a2a77459f37756c489018ee61310baf76
Matthieu Barthélemy <bonsouere@gmail.com>
2020-04-05 09:38:02 +0800

    Try creating new db eontext

--- Sources/QueuesFluentDriver/FluentQueuesDriver.swift
+++ Sources/QueuesFluentDriver/FluentQueuesDriver.swift
@@ -16,8 +16,8 @@ public struct FluentQueuesDriver {
 
 extension FluentQueuesDriver: QueuesDriver {
     public func makeQueue(with context: QueueContext) -> Queue {
-        
-        let db = self.database.configuration
+
+        let db = self.database.context.configuration
             .makeDriver(for: Databases.init(threadPool: NIOThreadPool.init(numberOfThreads: 4), on: context.eventLoop))
             .makeDatabase(with:
                 DatabaseContext(configuration: self.database.configuration, logger: self.database.logger, eventLoop: context.eventLoop)
