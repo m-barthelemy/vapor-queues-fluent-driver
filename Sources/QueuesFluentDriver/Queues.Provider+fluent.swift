fd5ea91e98169d161671402d1cbecad0f5edd219
Matthieu Barthélemy <bonsouere@gmail.com>
2020-04-07 00:28:42 +0800

    Remove need for dbType parameter

--- Sources/QueuesFluentDriver/Queues.Provider+fluent.swift
+++ Sources/QueuesFluentDriver/Queues.Provider+fluent.swift
@@ -10,7 +10,7 @@ extension Application.Queues.Provider {
     /// **WARNING**: if set to `false`, with any database engine other than Sqlite, a given job could be picked by multiple workers, unless you only have one single Queues worker/process.
     /// `useSkipLocked` is `true` by default and is supported on Mysql >= 8.0.1, MariaDB >= 10.3, Postgres >= 9.5, Oracle >= 9i(?).
     /// Sqlite doesn't have nor need it since it uses full table locking on update. Other dbs are just too weird (SQL Server).
-    public static func fluent(_ dbId: DatabaseID, dbType: QueuesFluentDbType, useSoftDeletes: Bool = true) -> Self {
+    public static func fluent(_ dbId: DatabaseID, useSoftDeletes: Bool = true) -> Self {
         .init {
             $0.queues.use(custom:
                 FluentQueuesDriver(on: dbId, useSoftDeletes: useSoftDeletes)
