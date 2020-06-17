dd118e907f9981f7c521309f2ab0a5bc1bda9250
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2020-06-17 12:56:53 +0200

    Update Fluent

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.3"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.1.0"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.1.0"),
     ],
